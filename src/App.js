51c9d24fc98e0b0f9a598b038837559a6c1f3e86
Vishwas Nahar <vshwsnahar3@gmail.com>
2020-05-03 19:44:03 +0530

    Update html changes in root path

--- src/App.js
+++ src/App.js
@@ -6,19 +6,34 @@ import TodosList from './components/create-todo.component'
 import EditTodo from './components/edit-todo.component'
 import CreateTodo from './components/todos-list.component'
 
-
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div class='container'>
-        <h2>MERN To Do App!</h2>
-      </div>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+          <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+        </a>
+        <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+        <div className="collpase navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Todos</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Todo</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <br/>
       <Route path="/" exact component={TodosList} />
       <Route path="/edit/:id" component={EditTodo} />
       <Route path="/create" component={CreateTodo} />
-    </Router>
+    </div>
+  </Router>
   );
 }
 
